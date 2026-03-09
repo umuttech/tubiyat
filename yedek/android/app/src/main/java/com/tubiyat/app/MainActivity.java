ffa7e493a98bb649183233ea42d2d5f1cbc1d62f
Umut Ozgu <erdalozgu310@gmail.com>
2026-03-10 00:33:56 +0300

    v3.0.2: Liderlik tablosu ve mobil ayarlar menusu duzeltildi

--- yedek/android/app/src/main/java/com/tubiyat/app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.tubiyat.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-}
--- yedek/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.tubiyat.app',
-  appName: 'Tubiyat',
-  webDir: 'www'
-};
-
-export default config;
